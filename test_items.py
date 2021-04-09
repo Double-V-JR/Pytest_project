53f962a374a5f5d92d86873ae0de49a5cc2c0725
Double-V-JR <v.egorov92@mail.ru>
2021-04-09 22:34:15 +0500

    test_items

---
+++ test_items.py
@@ -0,0 +1,6 @@
+def test_page_oscar(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.implicitly_wait(5)
+    browser.get(link)
+    btn = browser.find_elements_by_css_selector('[class="btn btn-lg btn-primary btn-add-to-basket"]')
+    assert btn, "Кнопка не найдена!"
